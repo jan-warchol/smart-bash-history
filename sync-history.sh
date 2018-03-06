398bd18247bebcf3639dfb44962f3c42d19882a4
Jan Warchoł <jan.warchol@gmail.com>
2018-03-06 20:15:03 +0100

    Initial version of the gist
    Extract my history configuration to separate file.

---
+++ sync-history.sh
@@ -0,0 +1,36 @@
+
+# shell history is very useful; keep many months of history
+export HISTFILESIZE=100000
+export HISTSIZE=100000
+export HISTCONTROL=ignoreboth
+shopt -s histappend   # don't overwrite history file after each session
+# I prefer to keep my history in my data folder so that it's backed up
+export HISTFILE="$HOME/data/bash-history-$DISAMBIG_SUFFIX"
+export HISTTIMEFORMAT="%d/%m/%y %T "
+
+# write session history to dedicated file and sync with other sessions, always
+# keeping history from current session on top.
+# Note that HISTFILESIZE shouldn't be too big, or there will be a noticeable
+# delay. A value of 100000 seems to work reasonable.
+update_history () {
+  history -a ${HISTFILE}.$$
+
+  history -c
+  history -r
+  for f in ${HISTFILE}.*; do
+    if [ $f != ${HISTFILE}.$$ ]; then
+      history -r $f
+    fi
+  done
+  history -r ${HISTFILE}.$$
+}
+
+# merge into main history file on bash exit (see trap below)
+merge_history () {
+  cat ${HISTFILE}.$$ >> $HISTFILE
+  rm ${HISTFILE}.$$
+}
+
+export PROMPT_COMMAND='update_history'
+trap merge_history EXIT
+
