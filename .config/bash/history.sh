5ec16a44c34bc69e2c7b224647aee724311ce185
Jan Warchoł <jan.warchol@gmail.com>
2018-04-20 21:55:56 +0200

    Move other history settings to one file
    This file was imported from a gist that was only about syncing history;
    however, I don't think it makes sense to have settings split
    unnaturally.

--- .config/bash/history.sh
+++ .config/bash/history.sh
@@ -1,3 +1,12 @@
+# enable keeping history timestamp and set format to ISO-8601
+export HISTTIMEFORMAT="%F %T "
+
+export HISTCONTROL=ignoreboth   # ignore duplicates and commands starting with space
+export HISTIGNORE="?:cd:-:..:ls:ll:bg:fg:vim:cim:g:g s:g d:g-"
+
+# disable terminal flow control key binding, so that ^S will search history forward
+stty -ixon
+
 # Synchronize history between bash sessions
 #
 # Make history from other terminals available to the current one. However,
