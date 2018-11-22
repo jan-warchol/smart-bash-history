6864b25d64de3bd3da0f504c591ed81ee0452a5f
Jan Warchoł <jan.warchol@gmail.com>
2018-11-22 16:16:45 +0100

    Simplify (remove nonessential functionality)
    Backup and autosplit features were cool, but not really necessary. It's
    simpler to split the file manually, and without splitting logic, there
    is nothing to guard $HISTFILE against (it's only appended to), so there
    doesn't have to be a backup.

--- sync-history.sh
+++ sync-history.sh
@@ -8,27 +8,26 @@
 # Since history is saved on each prompt, this additionally protects it from
 # terminal crashes.
 
+
 # keep unlimited shell history because it's very useful
 export HISTFILESIZE=-1
 export HISTSIZE=-1
 shopt -s histappend   # don't overwrite history file after each session
 
-# ensure we have a backup that is not older than an hour, just in case
-[ -z `find $HISTFILE.backup~ -mmin -60 2>/dev/null` ] &&
-  \cp --backup $HISTFILE $HISTFILE.backup~
 
 # on every prompt, save new history to dedicated file and recreate full history
 # by reading all files, always keeping history from current session on top.
 update_history () {
   history -a ${HISTFILE}.$$
   history -c
-  history -r
+  history -r  # load common history file
+  # load histories of other sessions
   for f in `ls ${HISTFILE}.[0-9]* 2>/dev/null | grep -v "${HISTFILE}.$$\$"`; do
     history -r $f
   done
-  history -r "${HISTFILE}.$$"
+  history -r "${HISTFILE}.$$"  # load current session history
 }
-if [[  "$PROMPT_COMMAND" != *update_history* ]]; then
+if [[ "$PROMPT_COMMAND" != *update_history* ]]; then
   export PROMPT_COMMAND="update_history; $PROMPT_COMMAND"
 fi
 
@@ -38,6 +37,7 @@ merge_session_history () {
   \rm ${HISTFILE}.$$
 }
 trap merge_session_history EXIT
+
 
 # detect leftover files from crashed sessions and merge them back
 active_shells=$(pgrep `ps -p $$ -o comm=`)
@@ -54,17 +54,3 @@ if [ -n "$orphaned_files" ]; then
   echo "done."
 fi
 
-# split history file if updating history takes more than 0.1 s
-split_history_file () {
-  echo "Archiving old bash history for better performance..."
-  archive_file="$HISTFILE.archive.$(date +%F.%H:%M:%S)"
-  split -n "l/2" "$HISTFILE" "$HISTFILE.split_"
-  \mv --backup "$HISTFILE.split_aa" "$archive_file"
-  \mv --backup "$HISTFILE.split_ab" "$HISTFILE"
-  # exclude timestamp comments when summarizing split
-  echo -n $(sed '/^#[0-9]\+$/d' "$archive_file" | wc | awk '{print $1}')
-  echo " entries archived to `basename $archive_file`"
-  echo $(sed '/^#[0-9]\+$/d' "$HISTFILE" | wc | awk '{print $1}') entries remaining.
-}
-begin=$(date +%s.%N); update_history; end=$(date +%s.%N)
-((`echo "$end - $begin > .1" | bc`)) && split_history_file
