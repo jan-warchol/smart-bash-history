cef45d1e7688f883703c3f65dc232494324d556d
Jan Warchoł <jan.warchol@gmail.com>
2019-09-23 18:43:42 +0200

    History: make scripts universal, independent of my own setup
    - use more typical paths
    - remove customizations relevant only to me
    - make more modular

--- filtering.sh
+++ filtering.sh
@@ -1,53 +1,57 @@
 #!/bin/bash
 
-# Groom bash history file, removing entries that are not interesting and
-# duplicated.
-if [ $# != 1 ]; then
-  echo "Wrong number of arguments."
-  echo "Usage: $0 <history file to process>"
-fi
+# Remove duplicates and uninteresting entries from supplied history file.
+filter_bash_history() {
+  if [ $# != 1 ]; then
+    echo "Wrong number of arguments."
+    echo "Usage: filter_bash_history <history file to process>"
+    return 1
+  fi
 
-cat "$1" |
+  cat "$1" |
 
-# Normalize commands for better deduplication
+  # Normalize commands for better deduplication ############
 
-# trim leading and trailing whitespace
-sed -r 's/^\s+//; s/\s+$//' |
-
-# normalize "git" to "g"
-sed 's/^git /g /' |
+  # trim leading and trailing whitespace
+  sed -r 's/^\s+//; s/\s+$//' |
 
 
-# Remove uninteresting commands
+  # Remove uninteresting commands ##########################
 
-# prepend placeholder timestamp to commands that didn't have one;
-# join timestamp and command into one line for further processing
-sed -n '/^#[0-9]*$/!{s/^/#0000000001 /; p}; /^#[0-9]*$/{N; s/\n/ /g; p;}' |
+  # prepend placeholder timestamp to commands that didn't have one;
+  # join timestamp and command into one line for further processing
+  sed -n '/^#[0-9]*$/!{s/^/#0000000001 /; p}; /^#[0-9]*$/{N; s/\n/ /g; p;}' |
 
-# remove short commands (up to 6 characters)
-sed -r '/^#[0-9]* .{0,6}$/d' |
+  # remove short commands (up to 6 characters)
+  sed -r '/^#[0-9]* .{0,6}$/d' |
 
-# remove short git commands
-sed -r '/^#[0-9]* g .{1,8}$/d' |
+  # remove short git commands
+  sed -r '/^#[0-9]* git .{1,8}$/d' |
 
-# remove short cd commands
-sed -r '/^#[0-9]* cd (\/home\/)?.{1,16}$/d' |
+  # remove short cd commands
+  sed -r '/^#[0-9]* cd (\/home\/)?.{1,16}$/d' |
 
-# remove man commands
-sed -r '/^#[0-9]* man (git )?.[a-z_\-]*$/d' |
+  # remove man commands
+  sed -r '/^#[0-9]* man (git )?.[a-z_\-]*$/d' |
 
 
-# Deduplicate
+  # Deduplicate ############################################
 
-# sort by timestamp, keeping untimestamped commands at the beginning in
-# original order
-sort --stable --key=1,1 |
+  # sort by timestamp, keeping untimestamped commands at the beginning in
+  # original order
+  sort --stable --key=1,1 |
 
-# remove duplicates (keeping *last* occurrence). See
-# https://stackoverflow.com/questions/39076336/bash-remove-duplicates-preserve-order
-tac | awk '!uniq[substr($0, 12)]++' | tac |
+  # remove duplicates (keeping *last* occurrence). See
+  # https://stackoverflow.com/questions/39076336/bash-remove-duplicates-preserve-order
+  tac | awk '!uniq[substr($0, 12)]++' | tac |
 
-# split entries in two lines again, remove placeholder timestamps
-sed -r 's/^(#[0-9]*) /\1\n/' | grep -v "^#0000000001$" > "$1.filtered"
+  # split entries in two lines again, remove placeholder timestamps
+  sed -r 's/^(#[0-9]*) /\1\n/' | grep -v "^#0000000001$" > "$1.filtered"
 
-echo Trimmed history file saved to "$1.filtered"
+  backup_path="$1.$(date +%F.%T)~"
+  \mv "$1" --no-clobber "$backup_path"
+  \mv "$1.filtered" --no-clobber "$1"
+
+  echo \"$1\" filtered.
+  echo Original history saved to "$backup_path"
+}
--- settings.sh
+++ settings.sh
@@ -1,10 +1,10 @@
-# I prefer to keep my history in my data folder so that it's backed up
-export HISTDIR="$HOME/data/history"
-export HISTFNAME="$DISAMBIG_SUFFIX"
-export HISTFILE="$HISTDIR/$HISTFNAME"
-export HISTMERGED="${HISTFILE}_$(date +%Y-%m)"
-export HISTBACKUP="${HISTDIR}-backup/${HISTFNAME}_$(date +%Y-%m).bak"
-mkdir -p $HISTDIR ${HISTDIR}-backup; touch $HISTMERGED
+# keep unlimited shell history because it's very useful
+export HISTFILESIZE=-1
+export HISTSIZE=-1
+shopt -s histappend   # don't overwrite history file after each session
+
+export HISTFILE="$HOME/.bash_history.$(hostname)"
+export HISTBACKUP="${HISTFILE}.bak"
 
 # enable keeping history timestamp and set format to ISO-8601
 export HISTTIMEFORMAT="%F %T "
@@ -15,18 +15,18 @@ export HISTIGNORE="?:cd:-:..:ls:ll:bg:fg:vim:cim:g:g s:g d:g-:hrn:hrn *:hrm *"
 # disable terminal flow control key binding, so that ^S will search history forward
 stty -ixon
 
-# keep unlimited shell history because it's very useful
-export HISTFILESIZE=-1
-export HISTSIZE=-1
-shopt -s histappend   # don't overwrite history file after each session
-
-
 # ensure we have a backup and verify that we didn't loose stuff
 if [[ -e $HISTBACKUP && \
-  `stat --printf="%s" $HISTMERGED` -lt `stat --printf="%s" $HISTBACKUP` ]]; then
+  `stat --printf="%s" $HISTFILE` -lt `stat --printf="%s" $HISTBACKUP` ]]; then
     echo Warning! It seems that history file shrank - verify the backup!
-    ls -hog $HISTMERGED $HISTBACKUP
+    ls -hog $HISTFILE $HISTBACKUP
   else
-    \cp $HISTMERGED $HISTBACKUP
+    \cp $HISTFILE $HISTBACKUP
 fi
 
+# load remaining history configuration files
+# (we must use full path and cannot rely on $0 because of sourcing quirks)
+repo_root=$(dirname ${BASH_SOURCE})
+source $repo_root/synchronization.sh
+source $repo_root/filtering.sh
+source $repo_root/entry-pruning.sh
