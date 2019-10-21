0b18aa9a298395f6c1a55fdbd0538ff602b815ac
Jan Warchoł <jan.warchol@gmail.com>
2019-10-21 13:15:01 +0200

    History: simplify filtering function

--- filtering.sh
+++ filtering.sh
@@ -1,52 +1,33 @@
 #!/bin/bash
 
-# Remove duplicates and uninteresting entries from supplied history file.
 filter_bash_history() {
   file="${1:-$HISTFILE}"
-  backup_path="$file.before-filtering.bak"
-  echo -n Filtering \"$file\"...
-
-  \mv "$file" "$backup_path"
-  cat "$backup_path" |
-
-  # Normalize commands for better deduplication ############
+  backup="$file.before-filtering.bak"
+  \mv "$file" "$backup"
 
   # trim leading and trailing whitespace
-  sed -r 's/^\s+//; s/\s+$//' |
+  cat "$backup" | sed -r 's/^\s+//; s/\s+$//' |
 
-  # prepend placeholder timestamp to commands that didn't have one;
-  # join timestamp and command into one line for further processing
-  sed -n '/^#[0-9]*$/!{s/^/#0000000001 /; p}; /^#[0-9]*$/{N; s/\n/ /g; p;}' |
+  # check whether commands have timestamps (man bash -> HISTTIMEFORMAT);
+  # put timestamp and command in one line to make sorting possible.
+  if grep -E "^#[0-9]{10}$" </dev/stdin >/dev/null; then
+    # squashes multiline commands
+    sed '1i\\' | tr '\n' ' ' | sed -r 's/ (#[0-9]{10}) /\n\1 /g' | sed '1d'
+  else  # add placeholder
+    sed -n '/^#[0-9]*$/!{s/^/#0000000000 /; p}' 
+  fi |
 
+  # remove trivial commands
+  sed -r '/^#[0-9]* .{,6}$/d' |
+  sed -r '/^#[0-9]* [a-Z0-9 _/.]{,12}$/d' |
 
-  # Remove uninteresting commands ##########################
-
-  # remove short commands (up to 6 characters)
-  sed -r '/^#[0-9]* .{0,6}$/d' |
-
-  # remove short git commands
-  sed -r '/^#[0-9]* git .{1,8}$/d' |
-
-  # remove short cd commands
-  sed -r '/^#[0-9]* cd (\/home\/)?.{1,16}$/d' |
-
-  # remove man commands
-  sed -r '/^#[0-9]* man (git )?.[a-z_\-]*$/d' |
-
-
-  # Deduplicate ############################################
-
-  # sort by timestamp, keeping untimestamped commands at the beginning in
-  # original order
+  # sort by timestamp
   sort --stable --key=1,1 |
-
-  # remove duplicates (keeping *last* occurrence). See
-  # https://stackoverflow.com/questions/39076336/bash-remove-duplicates-preserve-order
+  # deduplicate, keeping last occurrence (https://stackoverflow.com/a/39076527)
   tac | awk '!uniq[substr($0, 12)]++' | tac |
 
   # split entries in two lines again, remove placeholder timestamps
-  sed -r 's/^(#[0-9]*) /\1\n/' | grep -v "^#0000000001$" > "$file"
+  sed -r 's/^(#[0-9]*) /\1\n/' | grep -v "^#0000000000$" > "$file"
 
-  echo " done."
-  echo Original history saved to "$backup_path"
+  echo Done. Original history saved to \"$backup\".
 }
