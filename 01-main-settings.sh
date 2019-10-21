86205b219aeb2a535ef0b7b6326a9097e1e0f4e9
Jan Warchoł <jan.warchol@gmail.com>
2019-10-21 19:02:50 +0200

    History: port 01-main-settings to ZSH
    A bit rough on the edges. For now remaining modules are disabled.

--- 01-main-settings.sh
+++ 01-main-settings.sh
@@ -1,23 +1,21 @@
-# keep unlimited history and don't overwrite history file after each session
-export HISTFILESIZE=-1
-export HISTSIZE=-1
-shopt -s histappend
+# keep lots of  history and don't overwrite history file after each session
+export HISTSIZE=999999999
+export SAVEHIST=$HISTSIZE
+setopt append_history hist_ignore_space hist_ignore_dups
 
 # unique suffix -> simpler backups if you have many systems
 # export HISTFILE="$HOME/.bash_history.$(hostname)"
 export HISTBACKUP="${HISTFILE}.bak"
 
 # don't save duplicates and commands starting with space
-export HISTCONTROL="ignorespace:ignoredups"
+setopt hist_ignore_space hist_ignore_dups
 # as well as any one-letter commands and some common commands
-export HISTIGNORE="?:ls:la:ll:fg:hrn"
-
-# we cannot rely on $0 because of sourcing quirks
-repo_root=$(dirname ${BASH_SOURCE})
-export PATH="$PATH:$repo_root"
+export HISTORY_IGNORE="(?|ls|la|ll|fg|hrn}"
 
 # load remaining history configuration files (must use full path)
-source $repo_root/backup.sh
-source $repo_root/fuzzy-search.sh
-source $repo_root/synchronization.sh
-source $repo_root/filtering.sh
+repo_root=$(dirname $0)
+export PATH="$PATH:$repo_root"
+# source $repo_root/backup.sh
+# source $repo_root/fuzzy-search.sh
+# source $repo_root/synchronization.sh
+# source $repo_root/filtering.sh
